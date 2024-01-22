c27c47f845bace0d0cbe5836fbd565a68c899c7f
Mikhail Semianiuk <grey.strelok@gmail.com>
2024-01-22 10:05:20 +0100

    feat: solve 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs').promises;
+const path = require('path');
+const pathSourceCSS = path.join(__dirname, 'styles');
+const pathBundleCSS = path.join(__dirname, 'project-dist', 'bundle.css');
+
+async function readDirectory(directoryPath) {
+  const bundleArray = [];
+  try {
+    const filesFolders = await fs.readdir(directoryPath, {
+      withFileTypes: true,
+    });
+
+    for (const file of filesFolders) {
+      if (file.isDirectory()) {
+        continue;
+      }
+
+      const filePath = path.join(directoryPath, file.name);
+      const fileInfo = path.parse(filePath);
+
+      if (fileInfo.ext !== '.css') {
+        continue;
+      }
+
+      const fileContent = await fs.readFile(filePath, 'utf8');
+      bundleArray.push(fileContent);
+    }
+    await fs.writeFile(pathBundleCSS, bundleArray.join('\n'), 'utf8');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+readDirectory(pathSourceCSS);
